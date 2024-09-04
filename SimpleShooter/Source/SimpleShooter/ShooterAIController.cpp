bdfd5a88967aa72ec46dd7b8f7f67974cd11b711
kht9544 <137915554+kht9544@users.noreply.github.com>
2024-09-04 10:25:13 +0900

    FPS Source
    Add FPS Source

---
+++ SimpleShooter/Source/SimpleShooter/ShooterAIController.cpp
@@ -0,0 +1,41 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ShooterAIController.h"
+#include "Kismet/GameplayStatics.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "ShooterCharacter.h"
+
+
+void AShooterAIController::BeginPlay()
+{
+    Super::BeginPlay();
+
+    
+    APawn* PlayerPawn = UGameplayStatics::GetPlayerPawn(GetWorld(),0);
+    if(AIBehavior != nullptr)
+    {
+        RunBehaviorTree(AIBehavior);
+
+        GetBlackboardComponent()->SetValueAsVector(TEXT("StartLocation"),GetPawn()->GetActorLocation());
+        GetBlackboardComponent()->SetValueAsVector(TEXT("PlayerLocation"),PlayerPawn->GetActorLocation());
+    }
+   
+}
+
+void AShooterAIController::Tick(float DeltaSeconds)
+{
+    Super::Tick(DeltaSeconds);
+}
+
+bool AShooterAIController::IsDead() const
+{
+    AShooterCharacter* ControlledCharacter = Cast<AShooterCharacter>(GetPawn());
+    if(ControlledCharacter != nullptr)
+    {
+        return ControlledCharacter->IsDead();
+    }
+
+    return true;
+
+}
