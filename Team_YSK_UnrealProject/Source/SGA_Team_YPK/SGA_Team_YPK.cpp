d108a7e5de2cbe16db0f34930b2b69f277d61ac7
kht9544 <137915554+kht9544@users.noreply.github.com>
2024-09-05 11:35:03 +0900

    Move Folder
    Move Folder

--- Team_YSK_UnrealProject/Source/SGA_Team_YPK/SGA_Team_YPK.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "SGA_Team_YPK.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SGA_Team_YPK, "SGA_Team_YPK" );
